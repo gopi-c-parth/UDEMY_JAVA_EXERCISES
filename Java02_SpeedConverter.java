3b0dc3f5034e24a083f8e558e7f02a81940a89e8
gopi-c-parth <gopi.c.parth@gmail.com>
2026-02-21 15:42:07 +0530

    Update File: Java02_SpeedConverter.java

--- Java02_SpeedConverter.java
+++ Java02_SpeedConverter.java
@@ -1,3 +1,18 @@
-public class SpeedConverter_02 {
+public class Java02_SpeedConverter {
     
+    public static long toMilesPerHour (double kilometersPerHour){
+        return kilometersPerHour<0 ? -1 : Math.round (kilometersPerHour/1.609);
+    }
+    
+    public static void printConversion(double kilometersPerHour){
+        System.out.println(kilometersPerHour<0 ? "Invalid Value" : kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
+    }
+
+    public static void main(String[] args) {
+        printConversion(1.5);
+        printConversion(10.25);
+        printConversion(-5.6);
+        printConversion(25.42);
+        printConversion(75.114);
+    }
 }
