2ac8dc76fd8d58f40e7fd9983469d036a94f2456
gopi-c-parth <gopi.c.parth@gmail.com>
2026-03-07 13:48:33 +0530

    Upadte File: Java18_EvenDigitSum.java Solution

---
+++ Java18_EvenDigitSum.java
@@ -0,0 +1,24 @@
+public class Java18_EvenDigitSum {
+
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(252));
+        System.out.println(getEvenDigitSum(-22));
+    }
+    
+    public static int getEvenDigitSum(int number){
+        if(number<0){
+            return -1;
+        }
+        int sum = 0;
+        while (number !=0){
+            int digit = number %10 ;
+            number /= 10;
+            if (digit % 2 ==0){
+                sum += digit ;
+            }
+}
+        return sum;
+    }
+}
+    
