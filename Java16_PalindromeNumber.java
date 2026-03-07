4545ef17b75c538d7d9118c49c23e3a2102afc14
gopi-c-parth <gopi.c.parth@gmail.com>
2026-03-07 11:55:51 +0530

    Upadte File: Java16_PalindromeNumber.java Solution

---
+++ Java16_PalindromeNumber.java
@@ -0,0 +1,20 @@
+public class Java16_PalindromeNumber {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+    }
+
+    public static boolean isPalindrome (int num){
+        int reverse = 0;
+        int original = num;
+        while(num != 0){
+        reverse = reverse*10 + num%10 ;
+        num /=10;
+        }
+        return reverse == original ;
+        
+    }
+    
+}
